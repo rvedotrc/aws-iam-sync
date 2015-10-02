cc65dbade3b7b32265beaeb08ce5997dc6b23ba9
Rachel Evans <git@rve.org.uk>
2015-10-02 22:32:46 +0100

    sync-engine: reduce looping

--- sync-engine.js
+++ sync-engine.js
@@ -18,29 +18,32 @@ var sync = function (want, got, idFunc, equalFunc) {
     var wantMap = makeMap(want, idFunc);
     var gotMap = makeMap(got, idFunc);
 
-    var ans = {};
+    var ans = {
+        create: [],
+        noop: [],
+        update: []
+    };
 
-    ans.create = Object.keys(wantMap)
-        .filter(function (k) { return !Object.hasOwnProperty.apply(gotMap, [k]); })
+    Object.keys(wantMap)
         .sort()
-        .map(function (k) { return wantMap[k]; });
+        .map(function (k) {
+            if (!Object.hasOwnProperty.apply(gotMap, [k])) {
+                ans.create.push( wantMap[k] );
+            } else {
+                var w = wantMap[k];
+                var g = gotMap[k];
+                if (equal(w, g, equalFunc)) {
+                    ans.noop.push( { want: w, got: g } );
+                } else {
+                    ans.update.push( { want: w, got: g } );
+                }
+            }
+        });
 
     ans.delete = Object.keys(gotMap)
         .filter(function (k) { return !Object.hasOwnProperty.apply(wantMap, [k]); })
         .sort()
         .map(function (k) { return gotMap[k]; });
-
-    ans.update = Object.keys(wantMap)
-        .filter(function (k) { return Object.hasOwnProperty.apply(gotMap, [k]); })
-        .filter(function (k) { return !equal(gotMap[k], wantMap[k], equalFunc);  })
-        .sort()
-        .map(function (k) { return { got: gotMap[k], want: wantMap[k] }; });
-
-    ans.noop = Object.keys(wantMap)
-        .filter(function (k) { return Object.hasOwnProperty.apply(gotMap, [k]); })
-        .filter(function (k) { return equal(gotMap[k], wantMap[k], equalFunc);  })
-        .sort()
-        .map(function (k) { return { got: gotMap[k], want: wantMap[k] }; });
 
     return ans;
 };
