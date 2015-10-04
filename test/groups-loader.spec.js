71ebfa55ac19172055f5c2f24cb7a041bd14551a
Rachel Evans <git@rve.org.uk>
2015-10-04 13:55:06 +0100

    GroupsLoader (copy+paste from roles, for now)

---
+++ test/groups-loader.spec.js
@@ -0,0 +1,130 @@
+var Q = require('q');
+var assert = require('assert');
+var fs = require('fs');
+var sinon = require('sinon');
+
+var GroupsLoader = require('../groups-loader');
+var dir = GroupsLoader.dir;
+
+describe('GroupsLoader', function () {
+
+    var sandbox;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    var givenFiles = function (fileMap) {
+        sandbox.stub(fs, "readdir").withArgs(dir).yields(null, Object.keys(fileMap));
+        var s = sandbox.stub(fs, "readFile");
+
+        Object.keys(fileMap).map(function (filename) {
+            s.withArgs(dir+'/'+filename).yields(null, JSON.stringify(fileMap[filename]));
+        });
+    };
+
+    it('loads from an empty directory', function (mochaDone) {
+        givenFiles({});
+
+        GroupsLoader.getWanted()
+            .then(function (ans) {
+                assert.deepEqual(ans, []);
+                mochaDone();
+            })
+            .done();
+    });
+
+    it('loads groups', function (mochaDone) {
+        var specA = [ { name: 'Alice', path: '/team/', policies: [ 'team' ] } ];
+        var expectedA = {
+            GroupName: "Alice",
+            Path: "/team/",
+            attachedManagedPolicies: [
+                { PolicyName: 'modav.team' }
+            ],
+            inlinePolicies: [],
+        };
+
+        var specB = [ { name: 'Bob', path: '/team/', policies: [ 'team' ] } ];
+        var expectedB = {
+            GroupName: "Bob",
+            Path: "/team/",
+            attachedManagedPolicies: [
+                { PolicyName: 'modav.team' }
+            ],
+            inlinePolicies: [],
+        };
+
+        givenFiles({
+            'b.json': specB,
+            'a.json': specA
+        });
+
+        GroupsLoader.getWanted()
+            .then(function (ans) {
+                assert.deepEqual(ans, [expectedA, expectedB]);
+                mochaDone();
+            })
+            .done();
+    });
+
+    it('ignores things that are not called something.json', function (mochaDone) {
+        sandbox.stub(fs, 'readFile').throws();
+
+        sandbox.stub(fs, "readdir").withArgs(dir).yields(null, ['a.txt', 'some-thing.json']);
+
+        GroupsLoader.getWanted()
+            .then(function (ans) {
+                assert.deepEqual(ans, []);
+                mochaDone();
+            })
+            .done();
+    });
+
+    it('merges policies for each group', function (mochaDone) {
+        givenFiles({
+            'a.json': [
+                { name: 'r1', path: '/p/', policies: [ 'x', 'y' ] }
+            ],
+            'b.json': [
+                { name: 'r1', path: '/p/', policies: [ 'z', 'x', 'y' ] }
+            ]
+        });
+
+        GroupsLoader.getWanted()
+            .then(function (ans) {
+                assert.deepEqual(ans[0].attachedManagedPolicies, [
+                    { PolicyName: 'modav.x' },
+                    { PolicyName: 'modav.y' },
+                    { PolicyName: 'modav.z' }
+                ]);
+                mochaDone();
+            })
+            .done();
+    });
+
+    it('throws on path conflict for a group', function (mochaDone) {
+        givenFiles({
+            'a.json': [
+                { name: 'r1', path: '/p1/', policies: [ 'x', 'y' ] }
+            ],
+            'b.json': [
+                { name: 'r1', path: '/p2/', policies: [ 'z', 'x', 'y' ] }
+            ]
+        });
+
+        GroupsLoader.getWanted()
+            .then(function (ans) {
+                throw 'expected failure';
+            }, function (err) {
+                assert.equal(err, 'path mismatch for r1');
+                mochaDone();
+            })
+            .done();
+    });
+
+});
