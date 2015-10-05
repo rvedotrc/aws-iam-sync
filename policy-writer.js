8837613a8896ea0db897b81c80433f8157fd34ef
Rachel Evans <git@rve.org.uk>
2015-10-05 08:29:00 +0100

    Re-implemented policy-writer using sync-engine.  Respects --dry-run.

--- policy-writer.js
+++ policy-writer.js
@@ -5,93 +5,6 @@ var Q = require('q');
 var AwsDataUtils = require('./aws-data-utils');
 var SyncEngine = require('./sync-engine');
 
-// var doCreate = function (iam, key, detail) {
-//     console.log("Create policy", key, "as", JSON.stringify(detail));
-//     // TODO respect dryRun
-//     return AwsDataUtils.collectFromAws(iam, "createPolicy", { PolicyName: key, PolicyDocument: JSON.stringify(detail) });
-// };
-// 
-// var doConditionalUpdate = function (iam, key, gotDetail, wantDetail) {
-//     var activePolicyVersion = gotDetail.PolicyVersionList.filter(function (v) { return v.IsDefaultVersion; })[0];
-//     var activePolicy = JSON.parse(decodeURIComponent(activePolicyVersion.Document));
-// 
-//     if (gotDetail.Path !== '/') {
-//         throw "Unable to deal with policy "+key+" with path "+gotDetail.Path;
-//     } else if (deepEqual(activePolicy, wantDetail)) {
-//         // console.log("Nothing to update for role", key);
-//         return Q(true);
-//     } else {
-//         console.log("Update role", key);
-//         // TODO respect dryRun
-//         return deleteAllInactivePolicyVersions(iam, gotDetail)
-//             .then(function () {
-//                 return AwsDataUtils.collectFromAws(iam, "createPolicyVersion", {
-//                     PolicyArn: gotDetail.Arn,
-//                     PolicyDocument: JSON.stringify(wantDetail),
-//                     SetAsDefault: true
-//                 });
-//             });
-//     }
-// };
-// 
-// var deleteAllInactivePolicyVersions = function (iam, gotPolicy) {
-//     return Q.all(
-//         gotPolicy.PolicyVersionList.filter(function (pv) {
-//             return !pv.IsDefaultVersion;
-//         }).map(function (pv) {
-//             return Q.all([ gotPolicy.Arn, pv.VersionId ]).spread(function (arn, versionId) {
-//                 return AwsDataUtils.collectFromAws(iam, "deletePolicyVersion", { PolicyArn: arn, VersionId: versionId });
-//             });
-//         })
-//     );
-// };
-// 
-// var doCreateUpdate = function (iam, wanted, got) {
-//     var outOfScopeNames = Object.keys(wanted).sort().filter(isOutOfScope);
-//     if (outOfScopeNames.length > 0) {
-//         throw "Wanted policy name(s) out of scope: " + JSON.stringify(outOfScopeNames);
-//     }
-// 
-//     var promises = [];
-// 
-//     // Create any that are missing
-//     // Also, update any that exist, but whose policies don't match what we want
-//     Object.keys(wanted).sort().map(function (k) {
-//         var existing = got.PolicyMap[k];
-//         if (existing) {
-//             promises.push(
-//                 Q.all([ iam, k, existing, wanted[k] ]).spread(doConditionalUpdate)
-//             );
-//         } else {
-//             promises.push(
-//                 Q.all([ iam, k, wanted[k] ]).spread(doCreate)
-//             );
-//         }
-//     });
-// 
-//     return Q.all(promises);
-// };
-// 
-// var doDeleteRole = function (iam, gotPolicy) {
-//     console.log("Delete unwanted role " + gotPolicy.PolicyName);
-//     // TODO respect dryRun
-// 
-//     return deleteAllInactivePolicyVersions(iam, gotPolicy)
-//         .then(function () {
-//             return AwsDataUtils.collectFromAws(iam, "deletePolicy", { PolicyArn: gotPolicy.Arn });
-//         });
-// };
-// 
-// var doDelete = function (iam, wanted, got) {
-//     return Q.all(
-//         Object.keys(got.PolicyMap).filter(function (n) {
-//             return !isOutOfScope(n) && !wanted[n];
-//         }).map(function (n) {
-//             return Q.all([ iam, got.PolicyMap[n] ]).spread(doDeleteRole);
-//         })
-//     );
-// };
-
 var PolicyWriterSyncer = function (config, iam, syncOps, gotMapped) {
     this.config = config;
     this.iam = iam;
@@ -102,13 +15,36 @@ var PolicyWriterSyncer = function (config, iam, syncOps, gotMapped) {
     };
 };
 
+PolicyWriterSyncer.prototype.deleteAllInactivePolicyVersions = function (gotPolicy) {
+    var t = this;
+    return Q.all(
+        gotPolicy.PolicyVersionList.filter(function (pv) {
+            return !pv.IsDefaultVersion;
+        }).map(function (pv) {
+            return Q.all([ gotPolicy.Arn, pv.VersionId ]).spread(function (arn, versionId) {
+                return AwsDataUtils.collectFromAws(t.iam, "deletePolicyVersion", { PolicyArn: arn, VersionId: versionId });
+            });
+        })
+    );
+};
+
 PolicyWriterSyncer.prototype.doCreate = function (want) {
+    var t = this;
     if (!this.isInScope(want)) {
         throw "Refusing to create out-of-scope policy " + JSON.stringify(want);
     }
 
     console.log("Create policy", CanonicalJson(want, null, 2));
     if (this.config.dryRun) return;
+
+    return AwsDataUtils.collectFromAws(this.iam, "createPolicy", {
+        PolicyName: want.PolicyName,
+        Path: want.Path,
+        Description: want.Description,
+        PolicyDocument: JSON.stringify(want.PolicyDocument),
+    }).then(function (r) {
+        t.gotMapped.PolicyMap[r.Policy.PolicyName] = r.Policy;
+    });
 };
 
 PolicyWriterSyncer.prototype.doCreates = function () {
@@ -121,12 +57,28 @@ PolicyWriterSyncer.prototype.doCreates = function () {
 };
 
 PolicyWriterSyncer.prototype.doUpdate = function (e) {
+    var t = this;
+
     if (!this.isInScope(e.got)) {
         throw "Refusing to update out-of-scope policy " + JSON.stringify(e.got);
     }
 
     console.log("Update policy", CanonicalJson(e, null, 2));
     if (this.config.dryRun) return;
+
+    if (e.want.Path != e.got.Path) {
+        console.log("Update requires delete & create", CanonicalJson(e, null, 2));
+        return t.doDelete(e.got).then(function () { return t.doCreate(e.want); });
+    }
+
+    return t.deleteAllInactivePolicyVersions(e.got)
+        .then(function () {
+            return AwsDataUtils.collectFromAws(t.iam, "createPolicyVersion", {
+                PolicyArn: e.got.Arn,
+                PolicyDocument: JSON.stringify(e.want.PolicyDocument),
+                SetAsDefault: true,
+            });
+        });
 };
 
 PolicyWriterSyncer.prototype.doUpdates = function () {
@@ -146,10 +98,16 @@ PolicyWriterSyncer.prototype.doCreatesUpdates = function () {
 };
 
 PolicyWriterSyncer.prototype.doDelete = function (got) {
+    var t = this;
     if (!this.isInScope(got)) return;
 
     console.log("Delete policy", CanonicalJson(got, null, 2));
     if (this.config.dryRun) return;
+
+    return this.deleteAllInactivePolicyVersions(got)
+        .then(function () {
+            return AwsDataUtils.collectFromAws(t.iam, "deletePolicy", { PolicyArn: got.Arn });
+        });
 };
 
 PolicyWriterSyncer.prototype.doDeletes = function () {
