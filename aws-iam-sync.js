0e898ac12ebd7ef2b41f02401002f7f4c1ef4ba0
Rachel Evans <git@rve.org.uk>
2015-10-05 01:07:09 +0100

    Rewriting based on SyncEngine and constructors

--- aws-iam-sync.js
+++ aws-iam-sync.js
@@ -7,13 +7,15 @@ var PolicyLoader = require('./policy-loader');
 var PolicyWriter = require('./policy-writer');
 var RolesLoader = require('./roles-loader');
 var RolesWriter = require('./roles-writer');
+var UsersLoader = require('./users-loader');
 var UsersWriter = require('./users-writer');
+var GroupsLoader = require('./groups-loader');
 var GroupsWriter = require('./groups-writer');
 
 var wantedRoles = Q(true).then(RolesLoader.getWanted);
 var wantedPolicies = Q(true).then(PolicyLoader.getWanted);
-var wantedUsers = Q(null);
-var wantedGroups = Q(null);
+var wantedUsers = Q(true).then(UsersLoader.getWanted);
+var wantedGroups = Q(true).then(GroupsLoader.getWanted);
 
 Q.all([ wantedRoles, wantedPolicies, wantedUsers, wantedGroups ])
     .spread(ConsistencyChecker.checkConsistency)
@@ -28,29 +30,38 @@ Q.all([ wantedRoles, wantedPolicies, wantedUsers, wantedGroups ])
             .then(IAMCollector.getAccountAuthorizationDetails)
             .then(IAMCollector.mapAccountAuthorizationDetails);
 
-        var doWrites = function () {
-            var addPolicies = Q.all([ iam, wantedPolicies, gotMapped ]).spread(PolicyWriter.doCreateUpdate);
-            var addRoles = addPolicies.then(function () {
-                return Q.all([ iam, wantedRoles, gotMapped ]).spread(RolesWriter.doCreateUpdate);
-            });
-            var addGroups = addPolicies.then(function () {
-                return Q.all([ iam, wantedGroups, gotMapped ]).spread(GroupsWriter.doCreateUpdate);
-            });
-            var addUsers = Q.all([ addPolicies, addGroups ]).then(function () {
-                return Q.all([ iam, wantedUsers, gotMapped ]).spread(UsersWriter.doCreateUpdate);
-            });
+        var policySyncer = Q.all([ iam, wantedPolicies, gotMapped ]).spread(PolicyWriter.sync);
 
+        var doWrites = function () {
+            var addPolicies = policySyncer.post("doCreateUpdate");
+            var addRoles = Q(true);
+            var addUsers = Q(true);
+            var addGroups = Q(true);
+//             var addPolicies = Q.all([ iam, wantedPolicies, gotMapped ]).spread(PolicyWriter.doCreateUpdate);
+//             var addRoles = addPolicies.then(function () {
+//                 return Q.all([ iam, wantedRoles, gotMapped ]).spread(RolesWriter.doCreateUpdate);
+//             });
+//             var addGroups = addPolicies.then(function () {
+//                 return Q.all([ iam, wantedGroups, gotMapped ]).spread(GroupsWriter.doCreateUpdate);
+//             });
+//             var addUsers = Q.all([ addPolicies, addGroups ]).then(function () {
+//                 return Q.all([ iam, wantedUsers, gotMapped ]).spread(UsersWriter.doCreateUpdate);
+//             });
+// 
             return Q.all([ addPolicies, addRoles, addGroups, addUsers ]);
         };
 
         var doCleanup = function () {
-            var delUsers = Q.all([ iam, wantedUsers, gotMapped ]).spread(UsersWriter.doDelete);
-            var delGroups = delUsers.then(function () {
-                return Q.all([ iam, wantedGroups, gotMapped ]).spread(GroupsWriter.doDelete);
-            });
-            var delRoles = Q.all([ iam, wantedRoles, gotMapped ]).spread(RolesWriter.doDelete);
+            var delUsers = Q(true);
+            var delGroups = Q(true);
+            var delRoles = Q(true);
+//             var delUsers = Q.all([ iam, wantedUsers, gotMapped ]).spread(UsersWriter.doDelete);
+//             var delGroups = delUsers.then(function () {
+//                 return Q.all([ iam, wantedGroups, gotMapped ]).spread(GroupsWriter.doDelete);
+//             });
+//             var delRoles = Q.all([ iam, wantedRoles, gotMapped ]).spread(RolesWriter.doDelete);
             var delPolicies = Q.all([ delRoles, delUsers, delGroups ]).then(function () {
-                return Q.all([ iam, wantedPolicies, gotMapped ]).spread(PolicyWriter.doDelete);
+                return policySyncer.post("doDelete");
             });
 
             return Q.all([ delPolicies, delRoles, delGroups, delUsers ]);
--- policy-writer.js
+++ policy-writer.js
@@ -1,100 +1,156 @@
 var deepEqual = require('deep-equal');
+var CanonicalJson = require('canonical-json');
 var Q = require('q');
 
 var AwsDataUtils = require('./aws-data-utils');
+var SyncEngine = require('./sync-engine');
 
-var isOutOfScope = function (name) {
-    return !name.match(/^modav\./);
+// var doCreate = function (iam, key, detail) {
+//     console.log("Create policy", key, "as", JSON.stringify(detail));
+//     // TODO respect dryRun
+//     return AwsDataUtils.collectFromAws(iam, "createPolicy", { PolicyName: key, PolicyDocument: JSON.stringify(detail) });
+// };
+// 
+// var doConditionalUpdate = function (iam, key, gotDetail, wantDetail) {
+//     var activePolicyVersion = gotDetail.PolicyVersionList.filter(function (v) { return v.IsDefaultVersion; })[0];
+//     var activePolicy = JSON.parse(decodeURIComponent(activePolicyVersion.Document));
+// 
+//     if (gotDetail.Path !== '/') {
+//         throw "Unable to deal with policy "+key+" with path "+gotDetail.Path;
+//     } else if (deepEqual(activePolicy, wantDetail)) {
+//         // console.log("Nothing to update for role", key);
+//         return Q(true);
+//     } else {
+//         console.log("Update role", key);
+//         // TODO respect dryRun
+//         return deleteAllInactivePolicyVersions(iam, gotDetail)
+//             .then(function () {
+//                 return AwsDataUtils.collectFromAws(iam, "createPolicyVersion", {
+//                     PolicyArn: gotDetail.Arn,
+//                     PolicyDocument: JSON.stringify(wantDetail),
+//                     SetAsDefault: true
+//                 });
+//             });
+//     }
+// };
+// 
+// var deleteAllInactivePolicyVersions = function (iam, gotPolicy) {
+//     return Q.all(
+//         gotPolicy.PolicyVersionList.filter(function (pv) {
+//             return !pv.IsDefaultVersion;
+//         }).map(function (pv) {
+//             return Q.all([ gotPolicy.Arn, pv.VersionId ]).spread(function (arn, versionId) {
+//                 return AwsDataUtils.collectFromAws(iam, "deletePolicyVersion", { PolicyArn: arn, VersionId: versionId });
+//             });
+//         })
+//     );
+// };
+// 
+// var doCreateUpdate = function (iam, wanted, got) {
+//     var outOfScopeNames = Object.keys(wanted).sort().filter(isOutOfScope);
+//     if (outOfScopeNames.length > 0) {
+//         throw "Wanted policy name(s) out of scope: " + JSON.stringify(outOfScopeNames);
+//     }
+// 
+//     var promises = [];
+// 
+//     // Create any that are missing
+//     // Also, update any that exist, but whose policies don't match what we want
+//     Object.keys(wanted).sort().map(function (k) {
+//         var existing = got.PolicyMap[k];
+//         if (existing) {
+//             promises.push(
+//                 Q.all([ iam, k, existing, wanted[k] ]).spread(doConditionalUpdate)
+//             );
+//         } else {
+//             promises.push(
+//                 Q.all([ iam, k, wanted[k] ]).spread(doCreate)
+//             );
+//         }
+//     });
+// 
+//     return Q.all(promises);
+// };
+// 
+// var doDeleteRole = function (iam, gotPolicy) {
+//     console.log("Delete unwanted role " + gotPolicy.PolicyName);
+//     // TODO respect dryRun
+// 
+//     return deleteAllInactivePolicyVersions(iam, gotPolicy)
+//         .then(function () {
+//             return AwsDataUtils.collectFromAws(iam, "deletePolicy", { PolicyArn: gotPolicy.Arn });
+//         });
+// };
+// 
+// var doDelete = function (iam, wanted, got) {
+//     return Q.all(
+//         Object.keys(got.PolicyMap).filter(function (n) {
+//             return !isOutOfScope(n) && !wanted[n];
+//         }).map(function (n) {
+//             return Q.all([ iam, got.PolicyMap[n] ]).spread(doDeleteRole);
+//         })
+//     );
+// };
+
+var PolicyWriterSyncer = function (iam, syncOps, gotMapped) {
+    this.iam = iam;
+    this.syncOps = syncOps;
+    this.gotMapped = gotMapped;
+    this.isInScope = function (e) {
+        return e.PolicyName.match(/^modav\./);
+    };
 };
 
-var doCreate = function (iam, key, detail) {
-    console.log("Create policy", key, "as", JSON.stringify(detail));
-    // TODO respect dryRun
-    return AwsDataUtils.collectFromAws(iam, "createPolicy", { PolicyName: key, PolicyDocument: JSON.stringify(detail) });
-};
-
-var doConditionalUpdate = function (iam, key, gotDetail, wantDetail) {
-    var activePolicyVersion = gotDetail.PolicyVersionList.filter(function (v) { return v.IsDefaultVersion; })[0];
-    var activePolicy = JSON.parse(decodeURIComponent(activePolicyVersion.Document));
-
-    if (gotDetail.Path !== '/') {
-        throw "Unable to deal with policy "+key+" with path "+gotDetail.Path;
-    } else if (deepEqual(activePolicy, wantDetail)) {
-        // console.log("Nothing to update for role", key);
-        return Q(true);
-    } else {
-        console.log("Update role", key);
-        // TODO respect dryRun
-        return deleteAllInactivePolicyVersions(iam, gotDetail)
-            .then(function () {
-                return AwsDataUtils.collectFromAws(iam, "createPolicyVersion", {
-                    PolicyArn: gotDetail.Arn,
-                    PolicyDocument: JSON.stringify(wantDetail),
-                    SetAsDefault: true
-                });
-            });
-    }
-};
-
-var deleteAllInactivePolicyVersions = function (iam, gotPolicy) {
+PolicyWriterSyncer.prototype.doCreateUpdate = function () {
+    var w = this;
     return Q.all(
-        gotPolicy.PolicyVersionList.filter(function (pv) {
-            return !pv.IsDefaultVersion;
-        }).map(function (pv) {
-            return Q.all([ gotPolicy.Arn, pv.VersionId ]).spread(function (arn, versionId) {
-                return AwsDataUtils.collectFromAws(iam, "deletePolicyVersion", { PolicyArn: arn, VersionId: versionId });
-            });
+        w.syncOps.create.map(function (want) {
+            if (!w.isInScope(want)) {
+                throw "Refusing to create out-of-scope policy " + JSON.stringify(want);
+            }
+            console.log("TODO, create policy", CanonicalJson(want, null, 2));
+        })
+        .concat(
+            w.syncOps.update.map(function (e) {
+                if (!w.isInScope(e.got)) {
+                    throw "Refusing to update out-of-scope policy " + JSON.stringify(e.got);
+                }
+                console.log("TODO, update policy", CanonicalJson(e, null, 2));
+            })
+        )
+    );
+};
+
+PolicyWriterSyncer.prototype.doDelete = function () {
+    var w = this;
+    return Q.all(
+        w.syncOps.delete.map(function (got) {
+            if (w.isInScope(got)) {
+                console.log("TODO, delete policy", CanonicalJson(got, null, 2));
+            }
         })
     );
 };
 
-var doCreateUpdate = function (iam, wanted, got) {
-    var outOfScopeNames = Object.keys(wanted).sort().filter(isOutOfScope);
-    if (outOfScopeNames.length > 0) {
-        throw "Wanted policy name(s) out of scope: " + JSON.stringify(outOfScopeNames);
-    }
+var findCurrentPolicyVersion = function (p) {
+    return p.PolicyVersionList.filter(function (pv) { return pv.IsDefaultVersion; })[0];
+};
 
-    var promises = [];
-
-    // Create any that are missing
-    // Also, update any that exist, but whose policies don't match what we want
-    Object.keys(wanted).sort().map(function (k) {
-        var existing = got.PolicyMap[k];
-        if (existing) {
-            promises.push(
-                Q.all([ iam, k, existing, wanted[k] ]).spread(doConditionalUpdate)
-            );
-        } else {
-            promises.push(
-                Q.all([ iam, k, wanted[k] ]).spread(doCreate)
-            );
+var sync = function (iam, wanted, gotMapped) {
+    var syncOps = SyncEngine.sync(
+        wanted,
+        gotMapped.Policies,
+        function (e) { return e.PolicyName; },
+        function (w, g) {
+            return w.PolicyName === g.PolicyName &&
+                w.Path === g.Path &&
+                deepEqual(w.PolicyDocument, JSON.parse(decodeURIComponent(findCurrentPolicyVersion(g).Document)));
         }
-    });
-
-    return Q.all(promises);
-};
-
-var doDeleteRole = function (iam, gotPolicy) {
-    console.log("Delete unwanted role " + gotPolicy.PolicyName);
-    // TODO respect dryRun
-
-    return deleteAllInactivePolicyVersions(iam, gotPolicy)
-        .then(function () {
-            return AwsDataUtils.collectFromAws(iam, "deletePolicy", { PolicyArn: gotPolicy.Arn });
-        });
-};
-
-var doDelete = function (iam, wanted, got) {
-    return Q.all(
-        Object.keys(got.PolicyMap).filter(function (n) {
-            return !isOutOfScope(n) && !wanted[n];
-        }).map(function (n) {
-            return Q.all([ iam, got.PolicyMap[n] ]).spread(doDeleteRole);
-        })
     );
+
+    return new PolicyWriterSyncer(iam, syncOps, gotMapped);
 };
 
 module.exports = {
-    doCreateUpdate: doCreateUpdate,
-    doDelete: doDelete
+    sync: sync,
 };
